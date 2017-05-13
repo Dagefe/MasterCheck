45e5b6927dd3dab57d955aa0f6fa4878992cca1d
Daniel <dagefe@gmail.com>
2017-05-13 14:44:47 +0200

    Tabla empresa

--- DB/mastercheck.sql
+++ DB/mastercheck.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 10-05-2017 a las 19:51:48
+-- Tiempo de generación: 13-05-2017 a las 14:42:21
 -- Versión del servidor: 10.1.21-MariaDB
 -- Versión de PHP: 5.6.30
 
