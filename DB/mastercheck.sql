ac2e8e09dc9b0f321a001466a1c6c012ce1feb83
Daniel <dagefe@gmail.com>
2017-05-13 20:15:10 +0200

    base datos

--- DB/mastercheck.sql
+++ DB/mastercheck.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 13-05-2017 a las 20:05:36
+-- Tiempo de generación: 13-05-2017 a las 20:14:34
 -- Versión del servidor: 10.1.16-MariaDB
 -- Versión de PHP: 7.0.9
 
@@ -36,6 +36,49 @@ CREATE TABLE `Clientes` (
   `provincia` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Empresa`
+--
+
+CREATE TABLE `Empresa` (
+  `id` int(8) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `contrasena` varchar(100) NOT NULL,
+  `contacto` varchar(100) NOT NULL,
+  `movil` int(8) NOT NULL,
+  `pais` varchar(100) NOT NULL,
+  `poblacion` varchar(100) NOT NULL,
+  `cp` int(8) NOT NULL,
+  `direccion` varchar(100) NOT NULL,
+  `nombre_empresa` varchar(100) NOT NULL,
+  `sectot` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Ofertas`
+--
+
+CREATE TABLE `Ofertas` (
+  `id_oferta` int(8) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `imagen` blob NOT NULL,
+  `descripcion` varchar(200) NOT NULL,
+  `precio` int(8) NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_fin` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `Ofertas`
+--
+
+INSERT INTO `Ofertas` (`id_oferta`, `nombre`, `imagen`, `descripcion`, `precio`, `fecha_inicio`, `fecha_fin`) VALUES
+(1, 'naru', '', 'oferta cerveza', 2, '2017-05-13', '2017-05-31');
+
 --
 -- Índices para tablas volcadas
 --
@@ -47,6 +90,18 @@ ALTER TABLE `Clientes`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `Empresa`
+--
+ALTER TABLE `Empresa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `Ofertas`
+--
+ALTER TABLE `Ofertas`
+  ADD PRIMARY KEY (`id_oferta`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -55,6 +110,16 @@ ALTER TABLE `Clientes`
 --
 ALTER TABLE `Clientes`
   MODIFY `id` int(8) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `Empresa`
+--
+ALTER TABLE `Empresa`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `Ofertas`
+--
+ALTER TABLE `Ofertas`
+  MODIFY `id_oferta` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
