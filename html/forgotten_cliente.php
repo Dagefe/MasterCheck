1f6b2f6c88ad78f3fef905f63815d1fba640c74b
Daniel <dagefe@gmail.com>
2017-05-28 17:28:15 +0200

    Estilos

--- html/forgotten_cliente.php
+++ html/forgotten_cliente.php
@@ -17,24 +17,90 @@
 
 </head>
 <body>
-    <!-- Arbol de navegacion -->
-    <header>
+
+  <!-- Navbar -->
+  <div class="row">
+    <div class="col-xs-12 col-lg-12">
+      <div class="navbar-wrapper">
+        <div class="container">
+          <div class="navbar navbar-inverse">
+            <div class="container-fluid">
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                <a class="navbar-brand" href="../index.html">MASTERCHECK</a>
+              </div>
+
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                  <li class="active"><a href="#">Nose <span class="sr-only">(current)</span></a></li>
+                  <li><a href="#">Nose</a></li>
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                      <li><a href="#">Action</a></li>
+                      <li><a href="#">Another action</a></li>
+                      <li><a href="#">Something else here</a></li>
+                      <li role="separator" class="divider"></li>
+                      <li><a href="#">Separated link</a></li>
+                      <li role="separator" class="divider"></li>
+                      <li><a href="#">One more separated link</a></li>
+                    </ul>
+                  </li>
+                </ul>
+                <!--
+                <form class="navbar-form navbar-left">
+                  <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                  </div>
+                  <button type="submit" class="btn btn-default">Submit</button>
+                </form>
+              -->
+                <ul class="nav navbar-nav navbar-right">
+                  <li><a href="login_cliente.html">Particular</a></li>
+                  <li><a href="login_empresa.html">Empresa</a></li>
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                      <li><a href="#">Action</a></li>
+                      <li><a href="#">Another action</a></li>
+                      <li><a href="#">Something else here</a></li>
+                      <li role="separator" class="divider"></li>
+                      <li><a href="#">Separated link</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+          </div><!-- /.nav-inverse -->
+        </div><!-- /.container -->
+      </div><!-- /.navbar-wrapper -->
+    </div><!-- /.col -->
+  </div><!-- /.row -->
+
+
+
+    <!-- Arbol de navegacion
+
         <ol class="breadcrumb">
             <li class="glyphicon glyphicon-home"><a href="../index.html"> Inicio</a></li>
             <li><a href="login_cliente.html">Particular</a></li>
             <li class="active">Recuperacion password particular</li>
         </ol>
+    -->
 
-    </header>
 
-    <section>
-
-        <div class="container">
+        <div class="container-welcome">
             <form action="forgotten_cliente.php" method="post">
 
 		          <div class="row">
                 <div class="col-xs-12 col-lg-8">
-            		  <p>¿Has olvidado la contraseña?</p>
+
                     <div class="panel panel-primary">
             			    <div class="panel-heading">
                         <h3 class="panel-title text-center">Recuperación de contraseña</h3>
@@ -60,8 +126,6 @@
           </form>
         </div>
 
-
-    </section>
 
     <footer>
 
